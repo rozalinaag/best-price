f737e5d20c683ee9de2dfa30b780bda303434c8a
rozalinaag <59352861+rozalinaag@users.noreply.github.com>
2025-02-15 18:23:05 -0500

    Header

--- src/shared/ui/Header/Header.tsx
+++ src/shared/ui/Header/Header.tsx
@@ -1,3 +1,12 @@
+import css from './styles.module.css';
+import contentHeader from './img/contentHeader.png';
+
 export function Header() {
-  return <div></div>;
+  return (
+    <div className={css.wrapper}>
+      <div className="container">
+        <img src={contentHeader} />
+      </div>
+    </div>
+  );
 }
