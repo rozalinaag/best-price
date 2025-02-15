8e59eb4bbcdf8f12027058bc6e448dc1b9d4f9d9
rozalinaag <59352861+rozalinaag@users.noreply.github.com>
2025-02-15 17:35:01 -0500

    hook for fetching

--- src/shared/api.ts
+++
@@ -1,9 +0,0 @@
-import { Announcement, Income } from "./types"
-
-export function fetchAnnouncements() {
- return fetch("http://localhost:3001/announcements").then((res) => res.json() as Promise<Announcement[]>)
-}
-
-export function fetchIncomeFromOrders() {
-  return fetch("http://localhost:3001/income").then((res) => res.json() as Promise<Income[]>)
- }
---
+++ src/shared/api/apiBrief.ts
@@ -0,0 +1,21 @@
+import { Announcement, Income } from "../types/typesBrief"
+
+export async function fetchAnnouncements(): Promise<Announcement[]> {
+  const response = await fetch("http://localhost:3001/announcements");
+
+  if (!response.ok) {
+    throw new Error("Something is wrong with the server..");
+  }
+
+  return response.json();
+}
+
+export async function fetchIncomeFromOrders(): Promise<Income[]> {
+  const response = await fetch("http://localhost:3001/income");
+
+  if (!response.ok) {
+    throw new Error("Something is wrong with the server..");
+  }
+
+  return response.json();
+}
