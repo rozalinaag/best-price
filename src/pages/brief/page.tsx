580c00ec6105dfcba5e9d9ac7e3eaaffb3627353
rozalinaag <59352861+rozalinaag@users.noreply.github.com>
2025-02-15 20:17:17 -0500

    Title component

--- src/pages/brief/page.tsx
+++ src/pages/brief/page.tsx
@@ -1,11 +1,13 @@
 import { fetchAnnouncements } from '../../shared/api/apiBrief';
 import { useDataFetch } from '../../shared/hooks/useDataFetch';
+import { Title } from '../../shared/ui';
 
 export function BriefPage() {
   const [error, announcements, isPending] = useDataFetch(fetchAnnouncements);
 
   return (
     <main>
+      <Title>Сводка</Title>
       {isPending && <p>Loading...</p>}
       {error && <p style={{ color: 'red' }}>{error}</p>}
       {announcements &&
