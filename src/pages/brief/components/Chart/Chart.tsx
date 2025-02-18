8483b6aa3b8ef8527376a4a179f8d2dcb44c280f
rozalinaag <59352861+rozalinaag@users.noreply.github.com>
2025-02-18 18:03:33 -0500

    background for chart

--- src/pages/brief/components/Chart/Chart.tsx
+++ src/pages/brief/components/Chart/Chart.tsx
@@ -1,11 +1,12 @@
 import { BarChart, Bar, XAxis, Tooltip, ResponsiveContainer } from 'recharts';
+import css from './styles.module.css';
 
 const data = [
   { name: '21, сб', value: 40 },
   { name: '22, вс', value: 60 },
   { name: '23, пн', value: 80 },
   { name: '24, вт', value: 70 },
-  { name: '25, ср', value: 100 },
+  { name: '25, ср', value: null },
   { name: '26, чт', value: 90 },
   { name: '27, пт', value: 50 },
 ];
@@ -13,7 +14,14 @@ const data = [
 const CustomTooltip = ({ active, payload }: any) => {
   if (active && payload && payload.length) {
     return (
-      <div style={{ background: '#333', color: '#fff', padding: '5px', borderRadius: '5px' }}>
+      <div
+        style={{
+          background: '#2A2C36',
+          color: '#fff',
+          padding: '4px 8px',
+          borderRadius: '8px',
+        }}
+      >
         {payload[0].value}
       </div>
     );
@@ -23,11 +31,17 @@ const CustomTooltip = ({ active, payload }: any) => {
 
 export function Chart() {
   return (
-    <ResponsiveContainer width="100%" height={200}>
+    <ResponsiveContainer className={css.wrapper} width="100%" height={240}>
       <BarChart data={data}>
-        <XAxis dataKey="name" tick={{ fill: '#666' }} />
+        <XAxis dataKey="name" tick={{ fill: '#636570' }} axisLine={false} />
         <Tooltip content={<CustomTooltip />} />
-        <Bar dataKey="value" fill="#8EB359" radius={[5, 5, 0, 0]} barSize={40} />
+        <Bar
+          background={{ fill: '#F6F5FA' }}
+          dataKey="value"
+          fill="#8FBA3E"
+          radius={[8, 8, 8, 8]}
+          barSize={94}
+        />
       </BarChart>
     </ResponsiveContainer>
   );
